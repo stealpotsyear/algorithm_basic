63ef4bb45198032294af88d47b53af978a45fbb7
HK <load0117@gmail.com>
2018-01-13 00:09:02 +0900

    UVa 272 problem

---
+++ Problem272.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//UVa 272
+public class Problem272 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input ="";
+		boolean first = true;
+		while((input = bf.readLine()) != null) {
+			for(int i=0; i<input.length(); i++) {
+				if(input.charAt(i) =='"')
+					if(first) {
+						input = input.substring(0, i) + "``" + input.substring(i+1, input.length());
+						first = false;
+					}else {
+						input = input.substring(0, i) + "''" + input.substring(i+1, input.length());
+						first = true;
+					}
+			}
+			System.out.println(input);
+		}
+	}
+}
