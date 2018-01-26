6bdb70a75e91ff82f3dc256d764bf0cd26a9eb39
HK <load0117@gmail.com>
2018-01-26 15:04:04 +0900

    uva 1124번 입력 그대로 출력 간단하다.

---
+++ uva/Problem1124.java
@@ -0,0 +1,20 @@
+package uva;
+
+import java.util.Scanner;
+
+public class Problem1124 {
+	public static void main(String[] args) throws Exception{
+		// TODO 자동 생성된 메소드 스텁
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		
+		
+	}
+
+}
